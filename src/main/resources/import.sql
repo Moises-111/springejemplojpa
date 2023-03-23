ad799ce1eb9731972d83499a445fccfa4e6adb7a
Jose Moises Garnica Medina <jose.garnica@coppel.com>
2023-03-23 13:28:52 -0700

    Se sube proyecto springejemplojpa

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+/* Populate tables */
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1, 'juan', 'perez', 'juanperez@miemail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(2, 'luis', 'perez', 'luis.perez@miemail.com', '2017-08-28');
